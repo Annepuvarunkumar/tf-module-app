ffba9674fd80ddc417da21671bc70d03e0bb8cc2
varun <varunannepu123@gmail.com>
2024-01-04 17:52:35 +0530

    App Cluster

---
+++ locals.tf
@@ -0,0 +1,7 @@
+locals {
+  name_prefix   = "${var.env}-${var.component}"
+  tags          = merge(var.tags, { tf-module-name = "app" }, { env = var.env })
+
+}
+
+
---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "tags" {}
+variable "env" {}
+variable "vpc_id" {}
+variable "subnet_ids" {}
+variable "component" {}
+variable "ssh_ingress_cidr" {}
+variable "sg_ingress_cidr" {}
+variable "zone_id" {}
+variable "port" {}
+
+
