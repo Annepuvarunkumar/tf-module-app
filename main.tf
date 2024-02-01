2efd7628578019fd737edd024c5e49d7ec0a991d
varun <varunannepu123@gmail.com>
2024-02-01 20:59:37 +0530

    target group attachment

--- main.tf
+++ main.tf
@@ -97,11 +97,19 @@ resource "aws_lb_listener_rule" "main" {
   }
 }
 
-#resource "aws_lb_target_group" "public" {
-#  count    = var.component == "frontend" ? 1 : 0
-#  name     = "${local.name_prefix}-public"
-#  port     = var.port
-#  protocol = "HTTP"
-#  vpc_id   = var.vpc_id
-#}
+resource "aws_lb_target_group" "public" {
+  count    = var.component == "frontend" ? length(var.subnet_ids) : 0
+  name     = "${local.name_prefix}-public"
+  port     = var.port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "public" {
+  count            = data.dns_a_record_set.private_alb.addrs
+  target_group_arn = aws_lb_target_group.public[0].arn
+  target_id        = element(data.dns_a_record_set.private_alb.addrs, count.index )
+  port             = 80
+}
+
 
