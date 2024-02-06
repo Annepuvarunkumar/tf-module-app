d7f1194df1f74cf500c2e554efd1b12d149bfdf0
varun <varunannepu123@gmail.com>
2024-02-06 20:36:46 +0530

    userdata

--- userdata.sh
+++ userdata.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-yum install ansible -y &>>/opt/userdata.log
-#yum install ansible python3.11-pip.noarch -y &>>/opt/userdata.log
-#pip3 install botocore boto3 &>>/opt/userdata.log
-ansible-playbook -i localhost, -U https://github.com/Annepuvarunkumar/roboshop-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
+#yum install ansible -y &>>/opt/userdata.log
+
+sudo yum install ansible python3.11-pip.noarch -y &>>/opt/userdata.log
+sudo pip3 install botocore boto3 &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/Annepuvarunkumar/roboshop-ansible.git main.yml -e component=${component} -e env=${env} &>>/opt/userdata.log
 
 
 
