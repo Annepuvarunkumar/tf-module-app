38018d570d79a3466b49b8b308e4e7502e8f686a
varun <varunannepu123@gmail.com>
2024-02-19 21:14:35 +0530

    prometheus

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_security_group" "main" {
 #    to_port     = 9100
 #    protocol    = "tcp"
 #    cidr_blocks = var.monitoring_ingress_cidr
-  }
+#}
   egress {
     from_port        = 0
     to_port          = 0
